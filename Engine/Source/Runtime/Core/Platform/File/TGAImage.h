1039a012455c09dac60c2383ef695cbbb8cc69b6
chengzidu <chengzidu@bolegames.com>
2025-01-27 15:11:02 +0800

    Add image

--- Engine/Source/Runtime/Core/Platform/File/TGAImage.h
+++ Engine/Source/Runtime/Core/Platform/File/TGAImage.h
@@ -3,21 +3,5 @@
 
 #include "IImage.h"
 
-#pragma pack(push,1)
-struct TGAHeader {
-    uint8_t  idlength = 0;
-    uint8_t  colormaptype = 0;
-    uint8_t  datatypecode = 0;
-    uint16_t colormaporigin = 0;
-    uint16_t colormaplength = 0;
-    uint8_t  colormapdepth = 0;
-    uint16_t x_origin = 0;
-    uint16_t y_origin = 0;
-    uint16_t width = 0;
-    uint16_t height = 0;
-    uint8_t  bitsperpixel = 0;
-    uint8_t  imagedescriptor = 0;
-};
-#pragma pack(pop)
 
 #endif
